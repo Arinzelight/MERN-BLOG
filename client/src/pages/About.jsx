5bdc1d65f2e29156f696b16e3bbafa46cca960da
Arinzelight <arinzelight2@gmail.com>
2024-05-12 23:25:44 +0100

    add about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+export default function About() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About MichaelHarry's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to MichaelHarry's Blog! This blog was created by Ezinwa
+              Harry as a final project on altschool fullstack software
+              engineering training through 3mtt to share his thoughts and ideas
+              with the world about what . Ezinwa Harry is a passionate developer
+              who loves to write about technology, coding, and related topics.
+            </p>
 
-const About = () => {
-  return <div>About</div>;
-};
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages.Harry is always learning and exploring new technologies,
+              so be sure to check back often for new content!
+            </p>
 
-export default About;
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
