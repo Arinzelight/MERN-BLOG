68d40cd83ed3f2b407ba31f932c0a91e075a3ebf
Arinzelight <arinzelight2@gmail.com>
2024-05-12 14:18:19 +0100

    complete user image upload functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,9 +26,9 @@ const Header = () => {
           className="hidden lg:inline"
         />
       </form>
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+      {/* <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
-      </Button>
+      </Button> */}
       <div className="flex gap-2 md:order-2">
         <Button
           className="w-12 h-10 hidden sm:inline"
