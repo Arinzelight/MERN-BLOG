3cd906c2b3f5e02cacc0a60c868b0d4be27fa5c2
Arinzelight <arinzelight2@gmail.com>
2024-05-12 23:42:44 +0100

    update blog pages

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,8 +2,12 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
+import DashUsers from "../components/DashUsers";
+import DashComments from "../components/DashComments";
+import DashboardComp from "../components/DashboardComp";
 
-const Dashboard = () => {
+export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState("");
   useEffect(() => {
@@ -13,15 +17,22 @@ const Dashboard = () => {
       setTab(tabFromUrl);
     }
   }, [location.search]);
-
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
+        {/* Sidebar */}
         <DashSidebar />
       </div>
+      {/* profile... */}
       {tab === "profile" && <DashProfile />}
+      {/* posts... */}
+      {tab === "posts" && <DashPosts />}
+      {/* users */}
+      {tab === "users" && <DashUsers />}
+      {/* comments  */}
+      {tab === "comments" && <DashComments />}
+      {/* dashboard comp */}
+      {tab === "dash" && <DashboardComp />}
     </div>
   );
-};
-
-export default Dashboard;
+}
