0917333fda3ce8e3ec93145becace8d02f2ed720
Arinzelight <arinzelight2@gmail.com>
2024-05-18 10:11:08 +0100

    display theme toggle on mobile view and make search bar visible

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,30 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
-import { Link, useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice";
 import { signoutSuccess } from "../redux/user/userSlice";
 import { useEffect, useState } from "react";
 
-const Header = () => {
+export default function Header() {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { theme } = useSelector((state) => state.theme);
   const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showMobileSearch, setShowMobileSearch] = useState(false);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   const handleSignout = async () => {
     try {
       const res = await fetch("/api/user/signout", {
@@ -28,33 +41,70 @@ const Header = () => {
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+    setShowMobileSearch(false); // Hide mobile search bar after submitting
+  };
+
   return (
-    <Navbar className="border-b-2 whitespace-nowrap text-sm sm:text-xl">
-      <Link to="/" className="self-center font-semibold dark:text-white">
-        <span className="px-2 py-l bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Michael
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Harry's
         </span>
-        Harry
+        Blog
       </Link>
-      <form>
+
+      {/* Desktop Search Form */}
+      <form onSubmit={handleSubmit} className="hidden lg:inline">
         <TextInput
           type="text"
-          placeholder="Search"
+          placeholder="Search..."
           rightIcon={AiOutlineSearch}
-          className="hidden lg:inline"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
-      {/* <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-        <AiOutlineSearch />
-      </Button> */}
-      <div className="flex gap-2 md:order-2">
+
+      {/* Mobile Search Button */}
+      {!showMobileSearch && (
         <Button
-          className="w-12 h-10 hidden sm:inline"
+          className="w-12 h-10 lg:hidden"
           color="gray"
           pill
-          onClick={() => {
-            dispatch(toggleTheme());
-          }}
+          onClick={() => setShowMobileSearch(true)}
+        >
+          <AiOutlineSearch />
+        </Button>
+      )}
+
+      {/* Mobile Search Input */}
+      {showMobileSearch && (
+        <form onSubmit={handleSubmit} className="w-full lg:hidden mt-2">
+          <TextInput
+            type="text"
+            placeholder="Search..."
+            rightIcon={AiOutlineSearch}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full"
+          />
+        </form>
+      )}
+
+      <div className="flex gap-2 md:order-2 items-center">
+        <Button
+          className="w-12 h-10"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
         >
           {theme === "light" ? <FaSun /> : <FaMoon />}
         </Button>
@@ -75,8 +125,8 @@ const Header = () => {
             <Link to={"/dashboard?tab=profile"}>
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
-            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
             <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to="/sign-in">
@@ -100,6 +150,4 @@ const Header = () => {
       </Navbar.Collapse>
     </Navbar>
   );
-};
-
-export default Header;
+}
