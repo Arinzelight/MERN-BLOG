0ef7e4c5dbe3f1fd6eefa569eac578c498b2e698
Arinzelight <arinzelight2@gmail.com>
2024-05-18 10:46:04 +0100

    add animation to homepage

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,42 @@ export default {
     "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        bounceFadeIn: "bounceFadeIn 2s ease-in-out",
+        wordReveal: "wordReveal 0.5s ease-in-out forwards",
+      },
+      keyframes: {
+        bounceFadeIn: {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(-50px)",
+          },
+          "50%": {
+            opacity: 0.5,
+            transform: "translateY(0)",
+          },
+          "70%": {
+            opacity: 0.7,
+            transform: "translateY(-20px)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(0)",
+          },
+        },
+        wordReveal: {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(0)",
+          },
+        },
+      },
+    },
   },
   plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
 };
